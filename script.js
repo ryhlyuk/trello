0882e7441ece4d013d5af50e8d1cafcb106a1546
F-18-02 <blabla@bla.bla>
2018-08-18 13:34:44 +0300

    Connect Roboto to the application and fix styles

--- script.js
+++ script.js
@@ -1,54 +1,33 @@
 (function () {
 	var group = document.getElementsByClassName('group')[0];
-
+	
+	function removeCard(group, event) {
+		var card = event.target.parentNode.parentNode;
+		group.removeChild(card);
+	}
+	
 	window.addCard = function () {
 		var defaultCard = document.createElement('div');
 		defaultCard.className = 'card';
 		var lastChild = group.children[group.children.length - 1];
 
+		var html = '<div>' +
+						'<progress value="33" max="100"></progress>' + 
+						'<i class="material-icons remove-card">clear</i>' + 
+					'</div>' + 
+					'<div> Title ' + (group.children.length - 1) + '</div>' + 
+					'<div class="additional-options">' +
+						'<icon class="material-icons">more_horiz</icon>' + 
+						'<div>' + 
+							'<icon class="material-icons">access_time</icon> ' + 
+							new Date().toDateString() +
+						'</div>' +
 
-		// progress adding
-		var progress = document.createElement('progress');
-		progress.setAttribute('value', '33');
-		progress.setAttribute('max', '100');
-		defaultCard.appendChild(document.createElement('div').appendChild(progress));
-
-		// title adding
-		var title = document.createElement('div');
-		title.innerText = 'Title ' + (group.children.length - 1);
-		defaultCard.appendChild(title);
-
-		// additional options adding
-		var additionalOptionsDiv = document.createElement('div');
-		additionalOptionsDiv.className = 'additional-options';
-		// icon adding
-		var icon = document.createElement('icon');
-		icon.className = 'material-icons';
-		icon.innerText = 'more_horiz';
-		additionalOptionsDiv.appendChild(icon);
-		// priority adding
-		var priority = document.createElement('div');
-		priority.className = 'priority-very-low';
-		additionalOptionsDiv.appendChild(priority);
-		// date adding
-		var dateWrapper = document.createElement('div');
-		var dateIcon = document.createElement('icon');
-		dateIcon.className = 'material-icons';
-		dateIcon.innerText = 'access_time';
-		dateWrapper.appendChild(dateIcon);
-		var now = new Date();
-		var date = document.createTextNode(now.toDateString());
-		dateWrapper.appendChild(date);
-		additionalOptionsDiv.appendChild(dateWrapper);
-		// avatar adding
-		var avatar = document.createElement('img');
-		avatar.setAttribute('src', 'https://pbs.twimg.com/profile_images/609783819/Dethklok_Avatar_by_argantes_400x400.jpg');
-		avatar.style.width = '50px';
-		avatar.className = 'avatar-mini';
-		avatar.style.height = '50px';
-		additionalOptionsDiv.appendChild(avatar);
-		defaultCard.appendChild(additionalOptionsDiv);
-
+						'<img src="https://pbs.twimg.com/profile_images/609783819/Dethklok_Avatar_by_argantes_400x400.jpg" class="avatar-mini">' + 
+					'</div>';
+		defaultCard.innerHTML = html;
+		var removeIcon = defaultCard.children[0].children[1];
+		removeIcon.addEventListener('click', removeCard.bind(this, group));
 		group.insertBefore(defaultCard, lastChild);
 	};
 }());
